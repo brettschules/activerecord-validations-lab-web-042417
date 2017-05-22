d988737882f2fd03b06b3ba7df68c26fc188d8e1
Brett Schules <brettschules@gmail.com>
2017-05-22 17:06:03 -0400

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: {minimum: 250 }
+  validates :summary, length: {maximum: 250}
+
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  # validates :title, exclusion: { in: ["True Facts"] }
+  validate :is_clickbaity?
+
+
+    def is_clickbaity?
+      words = ["Won't Believe", "Secret", "Top #{/[0-9]/}" ,"Guess"]
+      if title != nil
+
+      check_word = words.none? { |word| title.include?(word) }
+
+       errors.add(:title, "Not a Click Bait!") if check_word
+     end
+  end
 end
